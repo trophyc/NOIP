5b2f53d8e026a389cf7bab80e61eabf58bee6205
trophyc <trophyc@163.com>
2017-05-01 13:41:53 +0800

    count

---
+++ 2006/count/count.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string.h>
+#include <stdio.h>
+using namespace std;
+
+#define MAX_DIGITS 26
+#define MAX_TIMES 5
+
+int main ()
+{
+   int start, end, digits;
+   cin >> start >> end >> digits;
+   char jam[MAX_DIGITS];
+   cin >> jam;
+
+   for (int i = 0; i < digits; i++) {
+      jam[i] -= ('a' + start - 1);
+   }
+
+   for (int i = 0; i < MAX_TIMES; i++) {
+      int j = 0;
+      int c;
+      for (j = 0; j < digits; j++) {
+         if (jam[digits - 1- j] == end - start -j) {
+            // the last possible letter for this digit;
+            continue;
+         }
+         // the next letter for this digit;
+         c = ++ jam[digits -1 -j];
+         break;
+      }
+      j--;
+      for (; j >= 0; j --) {
+         jam[digits -1 -j] = ++c;
+      }
+
+      for (int k = 0; k < digits; k++) {
+         cout << (char)(jam[k] + 'a' + start -1);
+      }
+      cout << endl;
+   }
+
+   return 0;
+}
