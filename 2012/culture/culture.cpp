5a8050483ac42fd88ac88594013c89d323ebc476
trophyc <trophyc@163.com>
2017-06-16 22:28:51 +0800

    culture

---
+++ 2012/culture/culture.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#define MAX_NATIONS 100
+#define MAX_CULTURES 100
+
+int nationNbr, cultureNbr, roadNbrs, start, end;
+
+int main ()
+{
+   scanf ("%d %d %d %d %d", &nationNbr, &cultureNbr, &roadNbrs, &start, &end);
+   
+   return 0;
+}
