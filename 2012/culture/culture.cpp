3d47e4ff21667a516a287a30351399117941d5dd
trophyc <trophyc@163.com>
2017-06-17 19:57:39 +0800

    culture init

--- 2012/culture/culture.cpp
+++ 2012/culture/culture.cpp
@@ -1,13 +1,53 @@
 #include <stdio.h>
+#include <memory.h>
 
 #define MAX_NATIONS 100
 #define MAX_CULTURES 100
+#define MIN(a,b) ((a) < (b) ? (a) : (b))
 
-int nationNbr, cultureNbr, roadNbrs, start, end;
+int nationNbr, cultureNbr, roadNbr, start, end;
+
+struct Nation 
+{
+   int culture;
+   int roads [MAX_NATIONS];
+};
+
+Nation nations[MAX_NATIONS];
+bool cultureExpel[MAX_CULTURES][MAX_CULTURES];
+void InitData();
 
 int main ()
 {
-   scanf ("%d %d %d %d %d", &nationNbr, &cultureNbr, &roadNbrs, &start, &end);
-   
+   InitData();
    return 0;
+}
+
+void InitData()
+{
+   memset (nations, 0, sizeof (nations));   
+   scanf ("%d %d %d %d %d", &nationNbr, &cultureNbr, &roadNbr, &start, &end);
+   for (int i = 0; i < nationNbr; i++) {
+      int culture;
+      scanf ("%d", &culture);
+      nations[i].culture = culture - 1;
+   }
+   for (int i = 0; i < cultureNbr; i++) {
+      for (int j = 0; j < cultureNbr; j++) {
+         int a;
+         scanf ("%d", &a);
+         if ( a == 1) {
+            cultureExpel[i][j] = true;
+         } else {
+            cultureExpel[i][j] = false;            
+         }
+      }
+   }
+   for (int i = 0; i < roadNbr; i++) {
+      int a, b, len;
+      scanf ("%d %d %d", &a, &b, &len);
+      a--;
+      b--;
+      nations[a].roads[b] = nations[b].roads[a] = MIN (len, nations[a].roads[b] );
+   }
 }
