c9cc69124fe8b67b716f70ef4069e54221361202
trophyc <trophyc@163.com>
2017-06-24 16:49:42 +0800

    level done!

--- 2013/level/level.cpp
+++ 2013/level/level.cpp
@@ -1,4 +1,4 @@
-#include <iotream>
+#include <stdio.h>
 #include <queue>
 #include <memory.h>
 
@@ -9,54 +9,110 @@ using namespace std;
 
 int stops, trains;
 
+typedef queue<int> IntQ;
 struct Vertice {
    int inComings;
-   int outGoings[MAX_STOPS];
+   int outGoings[MAX_STOPS+1];
 };
 
-Vertice v[MAX_STOPS];
+Vertice v[MAX_STOPS+1];
+int levels;
 
 void InitData();
 void PrintGraph ();
+void TopologicalSort ();
+void AddingFirstLevelToQ (IntQ* q);
 
 int main ()
 {
    InitData ();
+   TopologicalSort ();
+   printf ("%d\n", levels);
    return 0;
 }
 
 void InitData()
 {
-   cin >> stops >> trains;
+   levels = 0;
+   scanf ("%d %d", &stops, &trains);
    memset (v, 0, sizeof(v));
 
-   for (int i = 1; i < trains; i++) {
+   for (int i = 0; i < trains; i++) {
       int numbers;
       int trainStop[MAX_STOPS];
       memset (trainStop, 0, sizeof (trainStop));
-      cin >> numbers;
-      for (int i = 0; i < numbers; i++) {
+      scanf ("%d", &numbers);
+      for (int j = 0; j < numbers; j++) {
          int tmp;
-         cin >> tmp;
-         tmp--;
-         trainStop[i] = tmp;
+         scanf ("%d", &tmp);
+         trainStop[j] = tmp;
       }
       
       int ptr = 0;
-      for (int i = trainStop[0]; i <= trainStop[numbers-1]; i++) {
-         if (i == trainStop[ptr]) {
+      for (int j = trainStop[0]; j <= trainStop[numbers-1]; j++) {
+         if (j == trainStop[ptr]) {
             ptr++;
             continue;
          }
-         for (int j = 0; j < numbers; j++) {
-            v[trainStop[j]].inComings ++;
-            v[i].outGoings[trainStop[j]] = 1;
+         for (int k = 0; k < numbers; k++) {
+            if (v[j].outGoings[trainStop[k]] != 1) {
+               v[trainStop[k]].inComings ++;
+               v[j].outGoings[trainStop[k]] = 1;
+            }
          }
       }  
    }
 }
 
+void TopologicalSort ()
+{
+   // with bfs algorithm.
+   IntQ q;
+   AddingFirstLevelToQ (&q);
+  
+   while (!q.empty()) {
+      int a = q.front();
+      q.pop();
+      if (a == -1) {
+         // current level done, adding a new seprator for nexxt level
+         if (!q.empty()) {
+            q.push (-1);
+         }
+         levels ++; 
+         continue;
+      }
+    
+      for (int i = 1; i <= stops; i++) {
+         int next = v[a].outGoings[i];
+         if (next) {
+            v[i].inComings --;
+            if (v[i].inComings == 0) {
+               q.push(i);
+             }
+         }
+      }
+   }
+}
+
+void AddingFirstLevelToQ (IntQ* q)
+{
+   for (int i = 1; i <= stops; i++) {
+      if (v[i].inComings == 0) {
+         q->push(i);
+      }
+   }
+   q->push(-1); // adding a separator.
+}
+
 void PrintGraph ()
 {
-   
+   for (int i = 1; i <= stops; i++) {
+      printf ("%d: incoming: %d, outGoings: ", i, v[i].inComings);
+      for (int j = 0; j < stops; j++) {
+         if (v[i].outGoings[j]) {
+            printf ("%d ", j);
+         }
+      }
+      printf ("\n");
+   }
 }
