64616e5af29063b9db51fc48ca8af2bcc2fba798
trophyc <trophyc@163.com>
2017-06-28 19:00:23 +0800

    submatrix

--- 2014/submatrix/submatrix.cpp
+++ 2014/submatrix/submatrix.cpp
@@ -1,21 +1,36 @@
 #include <stdio.h>
+#include <limits.h>
 
 #define MAX_ROWS 16
+#define MAX_COLS 16
 
 void EnumerateNext (int total, int select, int* result);
+int GetMinWith (int* selectedRows, int rows, int totalCols, int cols);
+int GetMinHCost (int ith, int col, int* selectedRows, int rows);
 void PrintRows (int* result, int rows);
+int matrix[MAX_ROWS][MAX_COLS];
 
 int main ()
 {
    int totalRows, totalCols, rows, cols;
-
+   int min = INT_MAX;
    scanf ("%d %d %d %d", &totalRows, &totalCols, &rows, &cols);
+   for (int i = 0; i < totalRows; i++) {
+      for (int j = 0; j < totalCols; j++) {
+         scanf ("%d", &matrix[i][j]);
+      }
+   }
+
    int selectedRows[MAX_ROWS];
    selectedRows[0] = -1;
    do {
       EnumerateNext (totalRows, rows, selectedRows);
-      PrintRows(selectedRows, rows);
+      if (selectedRows[0] != -1) {
+         int tmp = GetMinWith (selectedRows, rows, totalCols, cols);
+         min = (min < tmp ? min : tmp);
+      }
    } while (selectedRows[0] != -1);
+   printf ("%d\n", min);
    return 0;
 }
 
@@ -45,6 +60,55 @@ void EnumerateNext (int total, int select, int* result)
       result[select -i -1] = ++a;
    }
    return;
+}
+
+/* dp[i][j] stores the min value when col j is selected as the ith selected row
+*/
+
+#define ABS(a) ((a) >= 0 ? (a) : -(a))
+
+int dp[MAX_ROWS+1][MAX_COLS]; 
+int v_cost[MAX_COLS]; // the vertical values
+
+int GetMinWith (int* selectedRows, int rows, int totalCols, int cols)
+{
+   for (int j = 0; j < totalCols; j++) {
+      v_cost[j] = 0;
+      for (int i = 0; i < rows-1; i++) {
+         int row = selectedRows[i];
+         int nextRow = selectedRows[i+1];
+         v_cost[j] += ABS(matrix[row][j] - matrix[nextRow][j]); 
+      }
+   }
+
+   for (int j = 0; j < cols; j++) {
+      dp[1][j] = v_cost[j];
+   }
+   for (int i = 2; i <= cols; i++) {
+      for (int j = i - 1; j < totalCols; j++) {
+         dp[i][j] = v_cost[j] + GetMinHCost (i, j, selectedRows, rows);
+      }
+   }
+
+   int min = INT_MAX;
+   for (int j = cols - 1; j < totalCols; j++) {
+      min = (min < dp[cols][j] ? min : dp[cols][j]);
+   }
+   return min;
+}
+
+int GetMinHCost (int ith, int col, int* selectedRows, int rows)
+{
+   int min = INT_MAX;
+   for (int j = ith - 2; j < col; j++) {
+      int h = dp[ith - 1][j];
+      for (int i = 0; i < rows; i++) {
+         int row = selectedRows[i];
+         h += ABS (matrix[row][j] - matrix[row][col]);
+      }
+      min = (min < h ? min : h);
+   }
+   return min;
 }
 
 void PrintRows (int* result, int rows)
