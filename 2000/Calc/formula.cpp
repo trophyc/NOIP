58fb1d2a5d616d24e35d9d8fc4660cd43fcfed79
trophyc <trophyc@163.com>
2018-04-16 11:30:26 +0800

    fix bug

--- 2000/Calc/formula.cpp
+++ 2000/Calc/formula.cpp
@@ -14,7 +14,7 @@ struct Expr
 #define IsLowerCase(c) (c >='a' && c <= 'z')
 
 void GetExpr (char* buf, int len, int& i, Expr& e, char& var);
-double Calculate (Expr& l, Expr& r);
+float Calculate (Expr& l, Expr& r);
 
 int main ()
 {
@@ -29,38 +29,41 @@ int main ()
 	GetExpr(buf, strlen (buf), i, left, v);
 	i++;
 	GetExpr(buf, strlen(buf), i, right, v);
-	printf ("%s", buf);
-	printf ("Left, Number = %d, Factor = %d\n", left.number, left.factor);
-	printf ("Right, Number = %d, Factor = %d\n", right.number, right.factor);
-	printf ("%c = %lf\n", v, Calculate(left, right));
+
+	printf ("%c=%.3f\n", v, Calculate(left, right));
 	return 0;
 }
 
 void GetExpr (char* buf, int len, int& i, Expr& e, char& v)
 {
 	int tmp = 0;
-	
+	bool digitFlag = false;
 	char c = buf[i];
+
 	while (c != '=' && c != '\0' && i < len) {
 		if (c == '+') {
 			e.number  += tmp * e.sign;
 			e.sign = 1;
 			tmp = 0;
+			digitFlag = false;
 		} else if (c == '-') {
 			e.number += tmp * e.sign;
 			e.sign = -1;
 			tmp = 0;
+			digitFlag = false;			
 		} else if (IsLowerCase(c)) {
 			v = c;
-			if (tmp == 0) {
+			if (!digitFlag) {
 				e.factor += e.sign;
 			} else {
 				e.factor += tmp * e.sign;
 			}
 			tmp = 0;
+			digitFlag = false;				
 		} else if (IsDigit(c)) {
 			tmp *= 10;
 			tmp += c - '0';
+			digitFlag = true;
 		}
 		
 		i++;
@@ -72,9 +75,9 @@ void GetExpr (char* buf, int len, int& i, Expr& e, char& v)
 	e.number += tmp * e.sign;
 }
 
-double Calculate (Expr& l, Expr& r)
+float Calculate (Expr& l, Expr& r)
 {
-	double factor = l.factor - r.factor;
-	double number = r.number - l.number;
+	float factor = l.factor - r.factor;
+	float number = r.number - l.number;
 	return (number/factor);
 }
