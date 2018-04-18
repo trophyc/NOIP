ee88196c2e3067622df73aa6eddcb8f9c8903932
trophyc <trophyc@163.com>
2018-04-18 15:24:15 +0800

    rewrite maxproduct

--- 2000/MaxProduct/MaxProduct.cpp
+++ 2000/MaxProduct/MaxProduct.cpp
@@ -1,60 +1,59 @@
-#include <iostream>
-#include <stdlib.h>
 #include <stdio.h>
-#include <memory.h>
+#include <string.h>
 
-using namespace std;
+#define MAX_LEN 40
+#define MAX_NUM 6
 
-int DynamicPlanning (char* buf, int len, int* dp, int start, int numbers);
+unsigned long long result[MAX_LEN][MAX_NUM];
+int n, k;
+char input[MAX_LEN+1];
+
+void ReadIn();
+void PrintOut ();
+unsigned long long dfs(int end, int muls);
 
 int main ()
 {
-   int len, numbers;
-   
-   cin >> len >> numbers;
-   numbers ++;
-   char* buf;
-   
-   buf = (char*)malloc (len+1);
-   cin.getline(buf, len+1);
-   cin.getline(buf, len+1);
-   
-   int* dp = (int*)malloc (len*numbers* sizeof(int));
-   memset(dp, -1, len*numbers*sizeof(int));
-   
-   cout << DynamicPlanning (buf, len, dp, 0, numbers) << endl;
-   
-   free (buf);
-   free (dp);
+   memset (result, -1, sizeof (result));
+   ReadIn();
+   printf ("%lu\n", dfs (n, k));
    return 0;
 }
 
-int DynamicPlanning (char* buf, int len, int* dp, int start, int numbers)
+void ReadIn ()
 {
-   int idx = len * numbers + start;
-   if (dp[idx] >0 ) {
-      return dp[idx];
-   }
-   
-   int i;
-   int cur_number = 0;
-   if (numbers == 1) {
-      for (i = start; i < len; i++) {
-         cur_number *= 10;
-         cur_number += buf[i] - '0';
-      }
-      dp[idx] = cur_number;
-      return cur_number;
-   }
-
-   int max = 0;
-   for (i = start+1; i < len -numbers +1 ; i++) {
-      cur_number *= 10;
-      cur_number += buf[i-1] - '0';
-      int tmp = cur_number * DynamicPlanning (buf, len, dp, i, numbers -1);
-      max = (max > tmp ? max : tmp);
-   }
-   dp[idx] = max;
-   return max;
+   scanf ("%d", &n);
+   scanf ("%d", &k);
+   scanf ("%s", input);
 }
 
+void PrintOut ()
+{
+   printf ("n = %d, k = %d", n, k);
+   printf ("%s\n", input);
+}
+
+unsigned long long dfs(int end, int muls)
+{
+   if (end == 0) {
+      return 1;
+   }
+   if (result[end][muls] != -1) {
+      return result[end][muls];
+   }
+
+   unsigned long long max = 0;
+   for (int start = muls; start < end; start++)
+   {
+      unsigned long long cur = 0;
+      for (int i = start; i < end; i++) {
+         cur *= 10;
+         cur += input[i] - '0';
+      }
+      unsigned long long tmp = cur * dfs(start, muls-1);
+      max = (max > tmp ? max : tmp);
+   }
+
+   result[end][muls] = max;
+   return max;
+}
